98f7e87822d08a0577ef62fc34eca8ba9a5e71ef
Gogs <gogs@fake.local>
2015-07-07 19:54:21 -0500

    Trying to make a SWIG extension

---
+++ swig/simple_example/example.cpp
@@ -0,0 +1,10 @@
+#include "example.h"
+
+int Math::pi() const {
+	return _pi;
+}
+
+void Math::pi(int pi) {
+	_pi = pi;
+}
+
---
+++ swig/simple_example/example.h
@@ -0,0 +1,14 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+class Math {
+public:
+	int pi() const;
+	void pi(int pi);
+
+private:
+	int _pi;
+};
+
+#endif // EXAMPLE_H
+
